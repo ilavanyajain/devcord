8751ceca939a9bb6dd7154082a4b2b204826e617
ilavanyajain <lavanyajain2434@gmail.com>
2025-05-10 17:38:59 +0530

    Explicitly import register view to fix AttributeError

--- devcord/urls.py
+++ devcord/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from chat.views import register
 from chat import views as chat_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('register/', chat_views.register, name='register'),
+    path('register/', register, name='register'),
 ]
